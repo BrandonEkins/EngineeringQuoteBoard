610af8a5d068bf82a218cd5de3b5812cfa344f9a
Ironslacker <william.keele103@gmail.com>
2016-11-10 01:09:33 -0700

    finished mongoDB side of app

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -4,15 +4,6 @@ angular.module('app', ['ngMaterial'])
         $scope.calendar = [];
         $scope.currentMonth = {};
         $scope.daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        // $scope.calendar = [
-        //     { id: 0, name: "January", days: [], },
-        //     { id: 1, name: "Febuary", days: [], },
-        //     { id: 2, name: "March", days: [], },angular.module('app', ['ngMaterial'])
-    .controller('mainCtrl', ['$scope', '$mdDialog', '$http', function($scope, $mdDialog, $http) {
-        $scope.CalendarName = "";
-        $scope.calendar = [];
-        $scope.currentMonth = {};
-        $scope.daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         $scope.tempCalendarName = "";
         $scope.setDate = function() {
             var my_date = new Date();
@@ -79,7 +70,7 @@ angular.module('app', ['ngMaterial'])
 
             $mdDialog.show(confirm).then(function(result) {
                 $scope.CalendarName = result;
-                var url = "../getcalendar?q=" + result;
+                var url = "../calendar?q=" + result;
                 $http.get(url)
                     .then(function(response) {
                         $scope.calendar = response.data;
@@ -88,14 +79,14 @@ angular.module('app', ['ngMaterial'])
             });
         };
 
-        $http.get("../getnames").then(function(response) {
+        $http.get("../names").then(function(response) {
             $scope.names = response.data;
         });
 
         $scope.selectCalendar = function() {
             console.log($scope.tempCalendarName);
             $scope.CalendarName = $scope.tempCalendarName;
-            var url = "../getcalendar?q=" + $scope.tempCalendarName;
+            var url = "../calendar?q=" + $scope.tempCalendarName;
             $http.get(url)
                 .then(function(response) {
                     $scope.calendar = response.data;
@@ -104,7 +95,7 @@ angular.module('app', ['ngMaterial'])
                 });
         }
         $scope.saveCalendar = function() {
-            var inputUrl = "../savecalendar?q=" + $scope.CalendarName;
+            var inputUrl = "../calendar?q=" + $scope.CalendarName;
             $http({
                 method: "POST",
                 url: inputUrl,
@@ -116,134 +107,4 @@ angular.module('app', ['ngMaterial'])
             });
         }
 
-    }]);
-        //     { id: 3, name: "April", days: [], },
-        //     { id: 4, name: "May", days: [], },
-        //     { id: 5, name: "June", days: [], },
-        //     { id: 6, name: "July", days: [], },
-        //     { id: 7, name: "August", days: [], },
-        //     { id: 8, name: "September", days: [], },
-        //     { id: 9, name: "October", days: [], },
-        //     { id: 10, name: "November", days: [], },
-        //     { id: 11, name: "December", days: [], }
-        // ]
-        // $scope.day = {
-        //     date: 0,
-        //     dayOfWeek: "",
-        //     events: [] // work on this
-        // }
-        // $scope.daysInEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-        // $scope.currentDayOfWeek = 5;
-        // $scope.calcDays = function() {
-        //     for (var i = 0; $scope.calendar.length > i; i++) { //selects correct month
-        //         for (var j = 0; $scope.currentDayOfWeek > j; j++) {
-        //             $scope.calendar[i].days.push({
-        //                 date: null,
-        //                 dayOfWeek: j,
-        //                 events: []
-        //             });
-        //         }
-        //         for (var j = 1; $scope.daysInEachMonth[i] >= j; j++) {
-        //             $scope.calendar[i].days.push({ // adds days to day array
-        //                 date: j,
-        //                 dayOfWeek: $scope.currentDayOfWeek,
-        //                 events: []
-        //             });
-        //             if ($scope.currentDayOfWeek == 6) { // increments days of week
-        //                 $scope.currentDayOfWeek = 0;
-        //             } else {
-        //                 $scope.currentDayOfWeek += 1;
-        //             }
-        //         }
-
-        //     }
-        // }
-        // $scope.calcDays();
-        $scope.setDate = function() {
-            var my_date = new Date();
-            $scope.currentMonth = $scope.calendar[my_date.getMonth()];
-            $scope.curMonthInt = my_date.getMonth();
-        }
-
-        $scope.forwardMonth = function() {
-            $scope.curMonthInt += 1;
-            $scope.currentMonth = $scope.calendar[$scope.curMonthInt]
-        }
-        $scope.backMonth = function() {
-            $scope.curMonthInt -= 1;
-            $scope.currentMonth = $scope.calendar[$scope.curMonthInt]
-        }
-        $scope.isToday = function(date) {
-            var today = new Date();
-            return date == today.getDate() && $scope.currentMonth.id == today.getMonth();
-        }
-        $scope.compareHide = function(value) {
-            if (value == "end") {
-                if ($scope.currentMonth.id > 10)
-                    return true;
-                else
-                    return false;
-            } else {
-                if ($scope.currentMonth.id < 1)
-                    return true;
-                else
-                    return false;
-            }
-        }
-        $scope.addEvent = function() {
-            $scope.selected_event = {};
-            $scope.currentMonth.days[$scope.selected_date].events.push($scope.selected_event);
-        }
-        $scope.deleteEvent = function() {
-            var index = $scope.currentMonth.days[$scope.selected_date].events.indexOf($scope.selected_event);
-            $scope.currentMonth.days[$scope.selected_date].events.splice(index, 1);
-            $scope.selected_event = null;
-        }
-        $scope.showPrompt = function() {
-            // Appending dialog to document.body to cover sidenav in docs app
-            var confirm = $mdDialog.prompt()
-                .title('Calendar Name')
-                .placeholder('Name')
-                .textContent('This Application acts as a directory of calendars. Add events and save the calender and then reload the webpage and type in the same name to access the saved Calendar')
-                .ariaLabel('Name')
-                .ok('Submit');
-
-            $mdDialog.show(confirm).then(function(result) {
-                $scope.CalendarName = result;
-                var url = "../getcalendar?q=" + result;
-                $http.get(url)
-                    .then(function(response) {
-                        $scope.calendar = response.data;
-                        $scope.setDate();
-                    });
-                // $.getJSON(url, function(data) {
-                //         $scope.calendar = data;
-                //         $scope.setDate();
-                //     })
-                //     .done(function() { console.log('getJSON request succeeded!'); })
-                //     .fail(function(jqXHR, textStatus, errorThrown) {
-                //         console.log('getJSON request failed! ' + textStatus);
-                //         console.log("incoming " + jqXHR.responseText);
-                //     })
-                //     .always(function() {
-                //         console.log('getJSON request ended!');
-                //     })
-                //     .complete(function() { console.log("complete"); 
-                // });
-            });
-        };
-        $scope.showPrompt();
-        $scope.saveCalendar = function() {
-            var inputUrl = "../savecalendar?q=" + $scope.CalendarName;
-            $http({
-                method: "POST",
-                url: inputUrl,
-                data: $scope.calendar
-            }).then(function mySucces(response) {
-                console.log(response);
-            }, function myError(response) {
-                console.log(response);
-            });
-        }
     }]);
--- routes/index.js
+++ routes/index.js
@@ -3,84 +3,138 @@ var fs = require('fs');
 var router = express.Router();
 var body = require('body-parser');
 var request = require('request');
-global.calendars = [];
+var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
+
+mongoose.connect('mongodb://localhost/calendarDB'); //Connects to a mongo database called "calendarDB"
+
+var calendarSchema = mongoose.Schema({
+    user: String,
+    calendar: [{
+        id: Number,
+        name: String,
+        days:[{ date: Number, dayOfWeek: String, events: [{name: String, time: String, location: String}] }]
+    }]
+});
+
+var Calendar = mongoose.model('Calendar', calendarSchema); //Makes an object from that schema as a model
+
+var db = mongoose.connection; //Saves the connection as a variable to use
+db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
+db.once('open', function() { //Lets us know when we're connected
+  console.log('Connected');
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log("CA4 Server Running");
     res.sendFile('index.html', { root: 'public' });
 });
 
-router.post('/savecalendar', function(req, res) {
-    for (var i = 0; i < calendars.length; i++) { // checks database for pre existing calendar
-        if (calendars[i].user == req.query.q) {
-            calendars[i] = { user: req.query.q, calendar: req.body };
-        }
-    }
-    res.end('success');
-});
-
-router.get('/getnames', function(req,res,next) {
-    var names = [];
-    for(var i = 0; i < calendars.length; i++){
-        names.push(calendars[i].user);
-    }
-   res.status(200).json(names); 
-});
-
-
-router.get('/getcalendar', function(req, res, next) {
-    var profile = {};
-    var inDatabase = false;
-    for (var i = 0; i < calendars.length; i++) { // checks database for pre existing calendar
-        if (calendars[i].user == req.query.q) {
-            profile = calendars[i];
-            inDatabase = true;
-        }
-    }
-    if (inDatabase == false) {
-        var day = { date: 0, dayOfWeek: "", events: [] }
-        var daysInEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        profile = {
-            user: req.query.q,
-            calendar: [
-                { id: 0, name: "January", days: [], },
-                { id: 1, name: "Febuary", days: [], },
-                { id: 2, name: "March", days: [], },
-                { id: 3, name: "April", days: [], },
-                { id: 4, name: "May", days: [], },
-                { id: 5, name: "June", days: [], },
-                { id: 6, name: "July", days: [], },
-                { id: 7, name: "August", days: [], },
-                { id: 8, name: "September", days: [], },
-                { id: 9, name: "October", days: [], },
-                { id: 10, name: "November", days: [], },
-                { id: 11, name: "December", days: [], }
-            ]
-        };
-
-        var currentDayOfWeek = 5; //2016 starts on a thursday...
-        function calcDays() {
-            for (var i = 0; profile.calendar.length > i; i++) { //selects correct month
-                for (var j = 0; currentDayOfWeek > j; j++) {
-                    profile.calendar[i].days.push({ date: null, dayOfWeek: j, events: [] });
-                }
-                for (var j = 1; daysInEachMonth[i] >= j; j++) {
-                    profile.calendar[i].days.push({ date: j, dayOfWeek: currentDayOfWeek, events: [] });
-                    if (currentDayOfWeek == 6) { // increments days of week
-                        currentDayOfWeek = 0;
-                    } else {
-                        currentDayOfWeek += 1;
-                    }
+router.post('/calendar', function(req, res) {
+    console.log("Update Calendar");
+    Calendar.find(function(err,calendars) {
+        if (err) return console.error(err);
+        else {
+            var calendarFound = false;
+            for (var i = 0; i < calendars.length; i++) { // checks database for pre-existing calendar and updates
+                if (calendars[i].user == req.query.q) {
+                    calendars[i] = { user: req.query.q, calendar: req.body };
+                    calendarFound = true;
                 }
             }
-        };
-        calcDays();
-        global.calendars.push({ user: profile.user, calendar: profile.calendar });
-    }
+            if(!calendarFound){                         // if not pre-existing, create new
+                var newCalendar = new Calendar({ user: req.query.q, calendar: req.body });
+                newCalendar.save(function(err, post) { //[4]
+                    if (err) return console.error(err);
+                    console.log(post);
+                    res.sendStatus(200);
+                });
+            }
+            else{
+                clendars.save(function(err, post){
+                    if (err) return console.error(err);
+                    console.log(post);
+                    res.sendStatus(200);
+                })
+            }
+        }
+    })
+});
+
+router.get('/names', function(req,res,next) {
+    console.log("Get Names");
+    Calendar.find(function(err,calendars) {
+        if (err) return console.error(err);
+        else {
+            var names = [];
+            for(var i = 0; i < calendars.length; i++){
+                names.push(calendars[i].user);
+            }
+            res.status(200).json(names);
+        }
+    })
+});
 
 
-    res.status(200).json(profile.calendar);
+router.get('/calendar', function(req, res, next) {
+    console.log("Get Calendar");
+    Calendar.find(function(err,calendars) {
+        if (err) return console.error(err);
+        else {    
+            var profile = {};
+            var inDatabase = false;
+            for (var i = 0; i < calendars.length; i++) { // checks database for pre existing calendar
+                if (calendars[i].user == req.query.q) {
+                    profile = calendars[i];
+                    inDatabase = true;
+                }
+            }
+            if (inDatabase == false) {
+                var day = { date: 0, dayOfWeek: "", events: [] }
+                var daysInEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+                var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                profile = {
+                    user: req.query.q,
+                    calendar: [
+                        { id: 0, name: "January", days: [], },
+                        { id: 1, name: "Febuary", days: [], },
+                        { id: 2, name: "March", days: [], },
+                        { id: 3, name: "April", days: [], },
+                        { id: 4, name: "May", days: [], },
+                        { id: 5, name: "June", days: [], },
+                        { id: 6, name: "July", days: [], },
+                        { id: 7, name: "August", days: [], },
+                        { id: 8, name: "September", days: [], },
+                        { id: 9, name: "October", days: [], },
+                        { id: 10, name: "November", days: [], },
+                        { id: 11, name: "December", days: [], }
+                    ]
+                };
+
+                var currentDayOfWeek = 5; //2016 starts on a thursday...
+                function calcDays() {
+                    for (var i = 0; profile.calendar.length > i; i++) { //selects correct month
+                        for (var j = 0; currentDayOfWeek > j; j++) {
+                            profile.calendar[i].days.push({ date: null, dayOfWeek: j, events: [] });
+                        }
+                        for (var j = 1; daysInEachMonth[i] >= j; j++) {
+                            profile.calendar[i].days.push({ date: j, dayOfWeek: currentDayOfWeek, events: [] });
+                            if (currentDayOfWeek == 6) { // increments days of week
+                                currentDayOfWeek = 0;
+                            } else {
+                                currentDayOfWeek += 1;
+                            }
+                        }
+                    }
+                };
+                calcDays();
+            }
+
+
+            res.status(200).json(profile.calendar);
+        }
+    })
+
 });
 
 module.exports = router;
