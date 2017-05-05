cd16dd91bb30d34091b6b9434a010ad72f24bb44
Brandon Ekins <ekinsb@byu.local>
2017-05-05 10:51:30 -0600

    Initial commit

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -1,124 +1,100 @@
-angular.module('app', ['ngMaterial'])
+angular.module('app', ['ngMaterial', 'textAngular'])
     .controller('mainCtrl', ['$scope', '$mdDialog', '$http', function($scope, $mdDialog, $http) {
-        $scope.CalendarName = "";
-        $scope.calendar = [];
-        $scope.currentMonth = {};
-        $scope.daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        $scope.tempCalendarName = "";
-        $scope.setDate = function() {
-            var my_date = new Date();
-            $scope.currentMonth = $scope.calendar[my_date.getMonth()];
-            $scope.curMonthInt = my_date.getMonth();
-        }
-
-        $scope.forwardMonth = function() {
-            $scope.curMonthInt += 1;
-            $scope.currentMonth = $scope.calendar[$scope.curMonthInt]
-        }
-        $scope.backMonth = function() {
-            $scope.curMonthInt -= 1;
-            $scope.currentMonth = $scope.calendar[$scope.curMonthInt]
-        }
-        $scope.isToday = function(date) {
-            var today = new Date();
-            return date == today.getDate() && $scope.currentMonth.id == today.getMonth();
-        }
-        $scope.compareHide = function(value) {
-            if (value == "end") {
-                if ($scope.currentMonth.id > 10)
-                    return true;
-                else
-                    return false;
-            } else {
-                if ($scope.currentMonth.id < 1)
-                    return true;
-                else
-                    return false;
-            }
-        }
-        $scope.addEvent = function() {
-            $scope.selected_event = {};
-            $scope.currentMonth.days[$scope.selected_date].events.push($scope.selected_event);
-        }
-        $scope.deleteEvent = function() {
-            var index = $scope.currentMonth.days[$scope.selected_date].events.indexOf($scope.selected_event);
-            $scope.currentMonth.days[$scope.selected_date].events.splice(index, 1);
-            $scope.selected_event = null;
-        }
-        $scope.showBeginDialog = function() {
-            var confirm = $mdDialog.confirm()
-                .title('Welcome to the MEAN Calendar Web App')
-                .textContent('This application uses the MEAN stack to impliment a calendar choose wether you would like to access a previously created calendar or make a new one')
-                .ok('New Calendar')
-                .cancel('Select Saved Calendar')
-            $mdDialog.show(confirm).then(function() {
-                $scope.showNewPrompt();
-            }, function() {
-                $scope.CalendarName = '-1';
-                //hides dialog and then make the main page just display a drop down if there is no calendar information.
-            });
-        }
-        $scope.showBeginDialog();
-        $scope.showNewPrompt = function() {
-            // Appending dialog to document.body to cover sidenav in docs app
-            var confirm = $mdDialog.prompt()
-                .title('Calendar Name')
-                .placeholder('Name')
-                .textContent('Enter a name for this calendar')
-                .ariaLabel('Name')
-                .ok('Submit');
-
-            $mdDialog.show(confirm).then(function(result) {
-                $scope.CalendarName = result;
-                var url = "../calendar?q=" + result;
-                $http.get(url)
-                    .then(function(response) {
-                        $scope.calendar = response.data;
-                        $scope.setDate();
-                    });
+        $scope.showAddDialog = function(ev) {
+            $mdDialog.show({
+                contentElement: '#AddDialog',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
             });
         };
-
-        $http.get("../names").then(function(response) {
-            $scope.names = response.data;
-        });
-
-        $scope.selectCalendar = function() {
-            console.log($scope.tempCalendarName);
-            $scope.CalendarName = $scope.tempCalendarName;
-            var url = "../calendar?q=" + $scope.tempCalendarName;
-            $http.get(url)
-                .then(function(response) {
-                    $scope.calendar = response.data;
-                    $scope.setDate();
-
-                });
+        $scope.hideDialog = function() {
+            $mdDialog.hide();
+            $scope.newQuote = {
+                id: "",
+                quote: "",
+                users: "",
+                date: ""
+            };
         }
-        $scope.saveCalendar = function() {
-            var inputUrl = "../calendar?q=" + $scope.CalendarName;
+        $scope.newQuote = {
+            id: "",
+            quote: "",
+            users: "",
+            date: ""
+        };
+        $scope.getQuotes = function() {
+            $http.get("../quote").then(function(response) {
+
+                $scope.quotes = response.data;
+                for (var i = 0; $scope.quotes.length > i; i++) {
+                    $scope.quotes[i].date = new Date($scope.quotes[i].date);
+                }
+                $scope.quotes = $scope.quotes.slice().reverse();
+            });
+        }
+        $scope.getQuotes();
+        $scope.saveQuote = function(id) {
+            var tempData;
+            if (id == 'edit') {
+
+                tempData = $scope.editQuote;
+
+                var inputUrl = "../quote";
+            } else if (id == 'new') {
+                $scope.newQuote.date = Date();
+                tempData = $scope.newQuote;
+                var inputUrl = "../quote";
+            }
             $http({
                 method: "POST",
                 url: inputUrl,
-                data: $scope.calendar
-            }).then(function mySucces(response) {
+                data: tempData
+            }).then(function mySuccess(response) {
                 console.log(response);
                 $mdDialog.show(
                     $mdDialog.alert()
-                        .clickOutsideToClose(true)
-                        .title('Success!')
-                        .textContent('This calendar has been successfully saved!')
-                        .ok('OK')
+                    .clickOutsideToClose(true)
+                    .title('Success!')
+                    .textContent('This Quote has been successfully saved!')
+                    .ok('OK')
                 );
             }, function myError(response) {
                 console.log(response);
                 $mdDialog.show(
                     $mdDialog.alert()
-                        .clickOutsideToClose(true)
-                        .title('Error!')
-                        .textContent('There was an error when trying to save this calendar!')
-                        .ok('OK')
+                    .clickOutsideToClose(true)
+                    .title('Error!')
+                    .textContent('There was an error when trying to save this Quote!')
+                    .ok('OK')
                 );
             });
+            $scope.getQuotes();
+            $mdDialog.hide();
         }
+        $scope.showEditDialog = function(id, ev) {
+            $scope.editQuote = $scope.quotes[id];
+            $mdDialog.show({
+                contentElement: '#editDialog',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            });
+        }
+        $scope.deleteQuote = function(id, ev) {
+            var inputUrl = "../quote?id=" + $scope.quotes[id]._id;
 
+            $http({
+                method: "DELETE",
+                url: inputUrl,
+                data: $scope.quotes[id]
+            }).then(function mySuccess(response) {
+                console.log(response);
+
+            }, function myError(response) {
+                console.log(response);
+
+            });
+            $scope.getQuotes();
+        }
     }]);
--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,23 @@ var router = express.Router();
 var body = require('body-parser');
 var request = require('request');
 var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
+var url = require('url');
+mongoose.connect('mongodb://localhost/quoteBoardDB'); //Connects to a mongo database called "quoteBoardDB"
 
-mongoose.connect('mongodb://localhost/calendarDB'); //Connects to a mongo database called "calendarDB"
 
-var calendarSchema = mongoose.Schema({
-    user: String,
-    calendar: [{
-        id: Number,
-        name: String,
-        days:[{ date: Number, dayOfWeek: String, events: [{name: String, time: String, location: String}] }]
-    }]
+var QuoteSchema = mongoose.Schema({
+
+    date: String,
+    quote: String,
+    users: String
 });
-
-var Calendar = mongoose.model('Calendar', calendarSchema); //Makes an object from that schema as a model
+//test
+var Quotes = mongoose.model('Quotes', QuoteSchema); //Makes an object from that schema as a model
 
 var db = mongoose.connection; //Saves the connection as a variable to use
 db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
 db.once('open', function() { //Lets us know when we're connected
-  console.log('Connected');
+    console.log('Connected');
 });
 
 /* GET home page. */
@@ -30,108 +29,51 @@ router.get('/', function(req, res, next) {
     res.sendFile('index.html', { root: 'public' });
 });
 
-router.post('/calendar', function(req, res) {
-    console.log("Update Calendar");
-    Calendar.find(function(err,calendars) {
+router.post('/quote', function(req, res) { // save and edit quotes
+    console.log("Update Quote");
+    Quotes.find(function(err, allQuotes) {
         if (err) return console.error(err);
         else {
-            var calendarFound = false;
-            for (var i = 0; i < calendars.length; i++) { // checks database for pre-existing calendar and updates
-                if (calendars[i].user == req.query.q) {
-                    calendars[i] = { user: req.query.q, calendar: req.body };
-                    calendarFound = true;
+            var quoteFound = false;
+            for (var i = 0; i < allQuotes.length; i++) { // checks database for pre-existing quote and updates
+                if (allQuotes[i]._id == req.body._id) { // checks database based on ID. 
+                    allQuotes[i] = { date: req.body.date, quote: req.body.quote, users: req.body.users };
+                    quoteFound = true;
+                    Quotes.update({ _id: req.body._id }, allQuotes[i], function(err, post) { //
+                        if (err) return console.error(err);
+                        console.log(post);
+                        res.sendStatus(200);
+                    })
                 }
             }
-            if(!calendarFound){                         // if not pre-existing, create new
-                var newCalendar = new Calendar({ user: req.query.q, calendar: req.body });
-                newCalendar.save(function(err, post) { //[4]
+            if (!quoteFound) { // if not pre-existing, create new
+                if (allQuotes.length == 0) {
+                    var newQuote = new Quotes({ date: req.body.date, quote: req.body.quote, users: req.body.users });
+                } else {
+                    var newQuote = new Quotes({ date: req.body.date, quote: req.body.quote, users: req.body.users });
+                }
+                newQuote.save(function(err, post) { //[4]
                     if (err) return console.error(err);
                     console.log(post);
                     res.sendStatus(200);
                 });
             }
-            else{
-                clendars.save(function(err, post){
-                    if (err) return console.error(err);
-                    console.log(post);
-                    res.sendStatus(200);
-                })
-            }
         }
     })
 });
+router.delete('/quote', function(req, res) {
+    Quotes.deleteOne({ _id: req.query.id }, function(err, post) {
+        if (err) return console.error(err);
+        res.sendStatus(200);
+    })
 
-router.get('/names', function(req,res,next) {
-    console.log("Get Names");
-    Calendar.find(function(err,calendars) {
+})
+router.get('/quote', function(req, res, next) { //get list of all quotes
+    console.log("Get Quote");
+    Quotes.find(function(err, allQuotes) {
         if (err) return console.error(err);
         else {
-            var names = [];
-            for(var i = 0; i < calendars.length; i++){
-                names.push(calendars[i].user);
-            }
-            res.status(200).json(names);
-        }
-    })
-});
-
-
-router.get('/calendar', function(req, res, next) {
-    console.log("Get Calendar");
-    Calendar.find(function(err,calendars) {
-        if (err) return console.error(err);
-        else {    
-            var profile = {};
-            var inDatabase = false;
-            for (var i = 0; i < calendars.length; i++) { // checks database for pre existing calendar
-                if (calendars[i].user == req.query.q) {
-                    profile = calendars[i];
-                    inDatabase = true;
-                }
-            }
-            if (inDatabase == false) {
-                var day = { date: 0, dayOfWeek: "", events: [] }
-                var daysInEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-                var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-                profile = {
-                    user: req.query.q,
-                    calendar: [
-                        { id: 0, name: "January", days: [], },
-                        { id: 1, name: "Febuary", days: [], },
-                        { id: 2, name: "March", days: [], },
-                        { id: 3, name: "April", days: [], },
-                        { id: 4, name: "May", days: [], },
-                        { id: 5, name: "June", days: [], },
-                        { id: 6, name: "July", days: [], },
-                        { id: 7, name: "August", days: [], },
-                        { id: 8, name: "September", days: [], },
-                        { id: 9, name: "October", days: [], },
-                        { id: 10, name: "November", days: [], },
-                        { id: 11, name: "December", days: [], }
-                    ]
-                };
-
-                var currentDayOfWeek = 5; //2016 starts on a thursday...
-                function calcDays() {
-                    for (var i = 0; profile.calendar.length > i; i++) { //selects correct month
-                        for (var j = 0; currentDayOfWeek > j; j++) {
-                            profile.calendar[i].days.push({ date: null, dayOfWeek: j, events: [] });
-                        }
-                        for (var j = 1; daysInEachMonth[i] >= j; j++) {
-                            profile.calendar[i].days.push({ date: j, dayOfWeek: currentDayOfWeek, events: [] });
-                            if (currentDayOfWeek == 6) { // increments days of week
-                                currentDayOfWeek = 0;
-                            } else {
-                                currentDayOfWeek += 1;
-                            }
-                        }
-                    }
-                };
-                calcDays();
-            }
-
-
-            res.status(200).json(profile.calendar);
+            res.status(200).json(allQuotes);
         }
     })
 
